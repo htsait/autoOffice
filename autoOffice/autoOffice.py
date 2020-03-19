ba67cf8df30c12f844cf713986ef4e48287ce7d9
lmw0_0 <lmw0_0@aliyun.com>
2020-03-19 23:52:40 +0800

    myMacCommit

--- autoOffice/autoOffice.py
+++ autoOffice/autoOffice.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
+
+
+@app.route('/regist/', methods=['GET', 'POST'])
+def regist():
+    if request.method == 'GET':
+        return render_template('regist.html')
+    else:
+        pass
 
 
 if __name__ == '__main__':
